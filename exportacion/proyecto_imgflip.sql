c23ac0dba5947d7a241a4d2dc308137d5c464243
Dmardomcad <daniel.mariscal@iesrfaelalberti.es>
2022-11-03 00:55:43 +0100

    Base de datos exportada

--- exportacion/proyecto_imgflip.sql
+++ exportacion/proyecto_imgflip.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db
--- Tiempo de generación: 24-10-2022 a las 11:26:33
+-- Tiempo de generación: 02-11-2022 a las 23:54:45
 -- Versión del servidor: 10.9.3-MariaDB-1:10.9.3+maria~ubu2204
--- Versión de PHP: 8.0.23
+-- Versión de PHP: 8.0.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,73 +20,96 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto_imgflip`
 --
+CREATE DATABASE IF NOT EXISTS `proyecto_imgflip` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `proyecto_imgflip`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `meme`
+-- Estructura de tabla para la tabla `memes`
 --
 
-CREATE TABLE `meme` (
+DROP TABLE IF EXISTS `memes`;
+CREATE TABLE `memes` (
   `id` int(11) NOT NULL,
+  `nombrememe` varchar(60) NOT NULL,
   `ruta` varchar(100) NOT NULL,
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `memes`
+--
+
+INSERT INTO `memes` (`id`, `nombrememe`, `ruta`, `id_usuario`) VALUES
+(1, '021122185130.jpg', 'memes/021122185130.jpg', 1),
+(2, '021122185205.jpg', 'memes/021122185205.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuario` (
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(40) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `pwd` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `pwd`) VALUES
+(1, 'Jefe', 'Jefazo'),
+(2, 'pepe', 'pepon');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `meme`
+-- Indices de la tabla `memes`
 --
-ALTER TABLE `meme`
+ALTER TABLE `memes`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombrememe` (`nombrememe`),
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indices de la tabla `usuario`
+-- Indices de la tabla `usuarios`
 --
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre_unique` (`nombre`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `meme`
+-- AUTO_INCREMENT de la tabla `memes`
 --
-ALTER TABLE `meme`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `memes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `usuario`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `meme`
+-- Filtros para la tabla `memes`
 --
-ALTER TABLE `meme`
-  ADD CONSTRAINT `meme_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `memes`
+  ADD CONSTRAINT `memes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
